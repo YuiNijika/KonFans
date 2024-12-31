3b51af40acb4a0466a4eb4cd045a7d0fee12c9fc
鼠子 <Tomori@miomoe.cn>
2024-12-31 22:25:14 +0800

    增加下雪效果

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,48 +1,34 @@
 <script setup>
-import {
-  useRouter
-} from 'vue-router';
-import {
-  ref
-} from 'vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
 const debugMessage = ref('');
 
-const GoIndex = () => {
-  debugMessage.value = 'GoIndex clicked';
-  router.push('/');
-};
+const menuItems = ref([
+  { icon: 'home', text: '首页', route: '/' },
+  { icon: 'rss_feed', text: '博客', url: 'https://blog.miomoe.cn/' },
+  { icon: 'camera', text: '哔哩哔哩', url: 'https://space.bilibili.com/435502585' },
+  { subheader: '壁纸' },
+  { icon: 'child_care', text: '表情包', route: '/Meme' },
+  { icon: 'extension', text: '全部壁纸', route: '/Wallpaper' },
+  { icon: 'extension', text: '电脑壁纸', route: '/WallpaperPC' },
+  { icon: 'extension', text: '手机壁纸', route: '/WallpaperMobile' },
+  { icon: 'extension', text: '轻音图网壁纸', route: '/WallpaperSpace' },
+  { subheader: '其他' },
+  { icon: 'beenhere', text: 'OpenAPI', route: '/OpenAPI' },
+  { icon: 'sms', text: '前往留言', url: 'https://messages.miomoe.cn/' },
+  { icon: 'videocam', text: '观看轻音全集', url: 'https://messages.miomoe.cn/Video' }
+]);
 
-const GoWallpaper = () => {
-  debugMessage.value = 'GoWallpaper clicked';
-  router.push('/Wallpaper');
+const navigate = (item) => {
+  if (item.route) {
+    debugMessage.value = `${item.text} clicked`;
+    router.push(item.route);
+  } else if (item.url) {
+    window.open(item.url, '_blank');
+  }
 };
-
-const GoWallpaperPC = () => {
-  debugMessage.value = 'WallpaperPC clicked';
-  router.push('/WallpaperPC');
-};
-
-const GoWallpaperMobile = () => {
-  debugMessage.value = 'WallpaperMobile clicked';
-  router.push('/WallpaperMobile');
-};
-
-const GoWallpaperSpace = () => {
-  debugMessage.value = 'WallpaperSpace clicked';
-  router.push('/WallpaperSpace');
-};
-
-const GoMeme = () => {
-  debugMessage.value = 'Meme clicked';
-  router.push('/Meme');
-};
-
-const GoOpenAPI = () => {
-  debugMessage.value = 'GoOpenAPI clicked';
-  router.push('/OpenAPI');
-}
 </script>
 
 <template>
@@ -51,69 +37,22 @@ const GoOpenAPI = () => {
       <img src="/logo.png" class="mdui-img-fluid">
       <div class="mdui-divider"></div>
       <ul class="mdui-list">
-        <a @click="GoIndex">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
-            <div class="mdui-list-item-content">首页</div>
-          </li>
-        </a>
-        <a href="https://space.bilibili.com/435502585" target="_blank">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">camera</i>
-            <div class="mdui-list-item-content">哔哩哔哩</div>
-          </li>
-        </a>
-        <li class="mdui-subheader">壁纸</li>
-        <a @click="GoMeme">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">child_care</i>
-            <div class="mdui-list-item-content">表情包</div>
-          </li>
-        </a>
-        <a @click="GoWallpaper">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">extension</i>
-            <div class="mdui-list-item-content">全部壁纸</div>
-          </li>
-        </a>
-        <a @click="GoWallpaperPC">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">extension</i>
-            <div class="mdui-list-item-content">电脑壁纸</div>
-          </li>
-        </a>
-        <a @click="GoWallpaperMobile">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">extension</i>
-            <div class="mdui-list-item-content">手机壁纸</div>
-          </li>
-        </a>
-        <a @click="GoWallpaperSpace">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">extension</i>
-            <div class="mdui-list-item-content">轻音图网壁纸</div>
-          </li>
-        </a>
-        <li class="mdui-subheader">其他</li>
-        <a @click="GoOpenAPI">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">beenhere</i>
-            <div class="mdui-list-item-content">OpenAPI</div>
-          </li>
-        </a>
-        <a href="https://messages.miomoe.cn/" target="_blank">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">sms</i>
-            <div class="mdui-list-item-content">前往留言</div>
-          </li>
-        </a>
-        <a href="https://messages.miomoe.cn/Video" target="_blank">
-          <li class="mdui-list-item mdui-ripple">
-            <i class="mdui-list-item-icon mdui-icon material-icons">videocam</i>
-            <div class="mdui-list-item-content">观看轻音全集</div>
-          </li>
-        </a>
-    </ul>
-  </div>
+        <template v-for="(item, index) in menuItems" :key="index">
+          <li v-if="item.subheader" class="mdui-subheader">{{ item.subheader }}</li>
+          <a v-else-if="item.url" :href="item.url" target="_blank">
+            <li class="mdui-list-item mdui-ripple">
+              <i class="mdui-list-item-icon mdui-icon material-icons">{{ item.icon }}</i>
+              <div class="mdui-list-item-content">{{ item.text }}</div>
+            </li>
+          </a>
+          <a v-else @click="navigate(item)">
+            <li class="mdui-list-item mdui-ripple">
+              <i class="mdui-list-item-icon mdui-icon material-icons">{{ item.icon }}</i>
+              <div class="mdui-list-item-content">{{ item.text }}</div>
+            </li>
+          </a>
+        </template>
+      </ul>
+    </div>
   </div>
 </template>
